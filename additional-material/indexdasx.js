548d33add5c0429282a5eb5a327484ef2806d3e5
cronousz <yumaris.560@gmail.com>
2022-10-03 13:58:56 +0700

    add contri and apt.txt

--- additional-material/indexdasx.js
+++
@@ -1,6 +0,0 @@
-const navbarToggle = document.querySelector('.navbar-toggle input');
-const nav = document.querySelector('nav ul');
-
-navbarToggle.addEventListener('click', function(){
-  nav.classList.toggle('slide');
-});
