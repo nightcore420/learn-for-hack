069498fa7c6e0cf1d390a21e6ec41df31ca03ad8
yaelahgus <92226688+yaelahgus@users.noreply.github.com>
2022-10-04 20:11:24 +0700

    Create calcyaelahgus.js

---
+++ additional-material/calcyaelahgus.js
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Belajar Javascript Part 4 : Operator Aritmatika Pada Javascript</title>
+	</head>
+	<body>
+		<h1>Belajar Javascript Part 4 <br/> Operator Aritmatika Pada Javascript</h1>
+		<h2>www.malasngoding.com</h2>
+ 
+		<div id="penjumlahan"></div>				
+ 
+		<script>
+			var bilangan1 = 6;
+			var bilangan2 = 2;
+ 
+			var penjumlahan = bilangan1 + bilangan2;			
+ 
+			document.getElementById("penjumlahan").innerHTML = penjumlahan;			
+		</script>
+	</body>
+</html>
