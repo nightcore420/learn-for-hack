1a9c9333609258758aa5a585948e05cc30e1c529
dimaswsss <mas.dimas2910@gmail.com>
2022-10-11 15:32:04 +0700

    Add file r about Scraping Tribunnews

---
+++ scrape_tribunnews_new.R
@@ -0,0 +1,17 @@
+library(rvest)
+
+urltribun <- 'https://www.tribunnews.com/tag/prabowo-subianto'
+halaman_akhir <- 4
+
+map_df(1:halaman_akhir, function(i) {
+  
+  cat('.')
+  laman <- read_html(sprintf(urltribun, i))
+  
+  data.frame(
+    JudulBerita = html_text(html_nodes(laman,'.ln24')),
+    stringsAsFactors = FALSE
+  )
+}) -> BeritaTribunnewsCom
+
+View(BeritaTribunnewsCom)
