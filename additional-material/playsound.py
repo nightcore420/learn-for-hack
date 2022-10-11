343f58f102e5e597ff0cb2a4dab464cc6164b096
Muhammad Daffa <52967635+Villaneuvo@users.noreply.github.com>
2022-10-11 11:06:08 +0700

    Create playsound.py

---
+++ additional-material/playsound.py
@@ -0,0 +1,8 @@
+import winsound
+# bullet_sound = "C:/Users/muham/Documents/Pemrograman/Python/Program/Space_Invader/laser.wav"
+# playsound(bullet_sound)
+
+#Winsound
+winsound.PlaySound("C:/Users/muham/Documents/Pemrograman/Python/Program/Space_Invader/laser.wav", winsound.SND_ASYNC)
+
+delay = input("Press ENTER to finish")
